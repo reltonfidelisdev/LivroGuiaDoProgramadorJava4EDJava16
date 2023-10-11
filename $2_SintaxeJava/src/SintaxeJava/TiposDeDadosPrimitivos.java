24ffe41446780af671b911a0f811e8eda288debe
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-10-10 23:54:45 -0300

    Commit Capitulo 2
    Exercícios capítulos 2.6-2.6.6

---
+++ $2_SintaxeJava/src/SintaxeJava/TiposDeDadosPrimitivos.java
@@ -0,0 +1,39 @@
+package SintaxeJava;
+
+public class TiposDeDadosPrimitivos {
+
+	public static void main(String[] args) {
+
+		/*
+		 *  Inteiro:	Bytes/bit		Faixa de valores
+		 * 	byte		1/8				de -128 a +127
+		 * 	short		2/19			de -32.768 a +32.767
+		 * 	int			4/32			de -2.147.483.648 a -2.147.483.647 
+		 * 	long		8/64			
+		 *  
+		 *  Real:
+		 *  float		4/32			de IEEE 754 ±1,40129846432481707e-45 a 3,40282346638528860e+38
+		 *  double		8/64			de IEEE 754 ±4,94065645841246544e-324 a 1,79769313486231570e+308
+		 *   
+		 *  Caractere:
+		 *  char		2/16			de \u0000 a \uFFFF
+		 *  
+		 *  Lógico:
+		 *  boolean		1/8				true e false
+		 */
+		
+//		byte varByte = 127;
+//		var varShort = (short) 32.767;
+//		double varDouble = 70.6754125;
+//		
+//		char varChar = 'A';
+//		// Para o tipo char, use sempre aspas simples
+//		
+//		String varString = "String ou Ccadeia de caracteres";	
+//		// Para o tipo String, use sempre aspas duplas
+//
+//		boolean varBoolean = true; // aceita true ou false
+
+	}
+
+}
