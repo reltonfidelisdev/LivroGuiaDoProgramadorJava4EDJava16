7327db22c025df15b967092ba3e64c23c43ec294
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-10-11 18:10:52 -0300

    Commit capítulo 2
    Exercícios capítulos 2.6-2.6.6 | String multilinha

--- $2_SintaxeJava/src/SintaxeJava/TiposDeDadosPrimitivos.java
+++ $2_SintaxeJava/src/SintaxeJava/TiposDeDadosPrimitivos.java
@@ -5,34 +5,41 @@ public class TiposDeDadosPrimitivos {
 	public static void main(String[] args) {
 
 		/*
-		 *  Inteiro:	Bytes/bit		Faixa de valores
-		 * 	byte		1/8				de -128 a +127
-		 * 	short		2/19			de -32.768 a +32.767
-		 * 	int			4/32			de -2.147.483.648 a -2.147.483.647 
-		 * 	long		8/64			de ? até ? -> Pesquisa na internet
-		 *  
-		 *  Real:
-		 *  float		4/32			de IEEE 754 ±1,40129846432481707e-45 a 3,40282346638528860e+38
-		 *  double		8/64			de IEEE 754 ±4,94065645841246544e-324 a 1,79769313486231570e+308
-		 *   
-		 *  Caractere:
-		 *  char		2/16			de \u0000 a \uFFFF
-		 *  
-		 *  Lógico:
-		 *  boolean		1/8				true e false
+		 * Inteiro: Bytes/bit Faixa de valores byte 1/8 de -128 a +127 short 2/19 de
+		 * -32.768 a +32.767 int 4/32 de -2.147.483.648 a -2.147.483.647 long 8/64 de ?
+		 * até ? -> Pesquisa na internet
+		 * 
+		 * Real: float 4/32 de IEEE 754 ±1,40129846432481707e-45 a
+		 * 3,40282346638528860e+38 double 8/64 de IEEE 754 ±4,94065645841246544e-324 a
+		 * 1,79769313486231570e+308
+		 * 
+		 * Caractere: char 2/16 de \u0000 a \uFFFF
+		 * 
+		 * Lógico: boolean 1/8 true e false
 		 */
-		
+
 //		byte varByte = 127;
 //		var varShort = (short) 32.767;
 //		double varDouble = 70.6754125;
 //		
 //		char varChar = 'A';
 //		// Para o tipo char, use sempre aspas simples
-//		
-//		String varString = "String ou Ccadeia de caracteres";	
-//		// Para o tipo String, use sempre aspas duplas
 //
 //		boolean varBoolean = true; // aceita true ou false
+//		
+//		String varString = "String ou cadeia de caracteres";	
+//		// Para o tipo String, use sempre aspas duplas
+		// String multi linha
+		// As Strings multilinha usam três aspas duplas seguidas
+		// como delimitador e o conteúdo da váriavel deve ficar contido nas linhas
+		// intermediárias
+
+//		// String multilinha
+//		String multilinha = """
+//				Um texto vai aqui
+//				em várias linhas
+//				mas SEM usar caracteres espesciais
+//				""";
 
 	}
 
