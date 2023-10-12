e8aac55a85410d70d8227a7c4c5090c81801ab8f
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-10-12 20:43:59 -0300

    Create PalavrasReservadas.java
    Resumo do capítulo 2.7.1 Palavras reservadas

---
+++ $2_SintaxeJava/src/SintaxeJava/PalavrasReservadas.java
@@ -0,0 +1,32 @@
+package SintaxeJava;
+
+public class PalavrasReservadas {
+
+	public static void main(String[] args) {
+
+		/**
+		 * O Java possui um conjunto de palavras que indicam os nomes dos tipos primitivos
+		 * e outros elementos pertencentes à sintaxe dessa linguagem. Essas palavras são
+		 * reservadas e não podem ser utilizadas com outro fim.
+		 * 
+		 */
+		String palavrasReservadas = """
+		/**
+		 * Palavras reservadas
+		 * abstract		assert		boolean		break		byte		case
+		 * catch		char		class		continue	default		do
+		 * double		else		enum		extends		false		final
+		 * finally		float		for			if			implements	import
+		 * instanceof	int			interface	long		native		new
+		 * null			package		private		protected	public		return
+		 * short		static		strictfp	super		switch		synchronized
+		 * this			throw		throws		transient	true		try
+		 * var			void		volatile	while		yield
+		 * 
+		 */
+		 """;
+		System.out.println(palavrasReservadas);
+
+	}
+
+}
