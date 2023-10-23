d4861de5a989b7975a59d6bb1e72e5e7d661ad86
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-10-23 16:38:49 -0300

    Update OperadoresBitwise.java
    Capítulo 2.11.8

--- $2_SintaxeJava/src/SintaxeJava/OperadoresBitwise.java
+++ $2_SintaxeJava/src/SintaxeJava/OperadoresBitwise.java
@@ -3,7 +3,12 @@ package SintaxeJava;
 public class OperadoresBitwise {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		/**
+		 * Considere duas variáveis a e b, do typo byte com 8 bits de conteúdocada:
+		 * a8a7a6a5a4a3a2a1a0 e b8b7b6b5b4b3b2b1b0. 
+		 * Operações bitwise ( ou bit-a-bit) efetuam operações entre os bits dos seus operandos.
+		 */
 
 	}
 
