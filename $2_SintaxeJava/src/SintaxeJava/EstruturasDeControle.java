2ee30e9bee0535ca9eadec69d2e0b031c66a8a4f
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-10-25 20:43:21 -0300

    Create EstruturasDeControle.java
    Capítulo 2.12 -> Introduz o leitor aos comandos condicionais e estrutura de controle do Java.

---
+++ $2_SintaxeJava/src/SintaxeJava/EstruturasDeControle.java
@@ -0,0 +1,21 @@
+package SintaxeJava;
+
+public class EstruturasDeControle {
+
+	public static void main(String[] args) {
+
+		/**
+		 * Um programa de computador é um conjunto de instruções organizadas para
+		 * produzir a solução de um problema. Naturalmente as instruções de um 
+		 * programa são executadas em sequência, o que se denomina fluxo sequencial de execução.
+		 * No entanto, em inúmeras circustânceas, é necessário executá-las em uma ordem 
+		 * diferente da estritamente sequencial. Essas situações são caracterizadas pela
+		 * necessidade da repetição de instruções e também pelo desvio de fluxo de execução,
+		 * tarefas que podem ser realizadas pelas estruturas de controle da linguagem.
+		 * 
+		 * Nos próximos capítulos, entraremos mais a fundo sobre isso.
+		 */
+
+	}
+
+}
