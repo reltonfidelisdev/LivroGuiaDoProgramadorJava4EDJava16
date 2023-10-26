eaac1d49863a0284489fa9b6077160890818acc6
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-10-25 21:38:37 -0300

    Create DiretivasBlocos.java
    Capítulo 2.12.1 -> introdução as diretivas e blocos

---
+++ $2_SintaxeJava/src/SintaxeJava/DiretivasBlocos.java
@@ -0,0 +1,51 @@
+package SintaxeJava;
+
+import java.util.Locale;
+
+public class DiretivasBlocos {
+
+	 private static String diretivaUm(String var) {
+		return var.toUpperCase();
+	}
+	public static void main(String[] args) {
+
+		/**
+		 * Antes de tratar das estruturas de controle são necessárias algumas definições.
+		 * Formalmente as instrunções de um programa são chamadas de diretivas (statements), as
+		 * qauis são tradiocionalmente escritas uma após a outra e separadas, por exemplo,
+		 * com uma quebra de linha ou um caractere de pontuação.
+		 * Em Java, assim como na linguagem C/C++/C#, as diretivas são separadas umas das
+		 * outras pelo símbolo de pontuação ; (ponte vírgila), sendo possível existir várias
+		 * diretivas numa mesma linha desde que separadas por ;.
+		 * 
+		 * Exemplo:
+		 * 	diretivaA; diretivaB;
+		 * 	diretivaC;
+		 * 	...
+		 * 	diretivaN;
+		 * 
+		 * As diretivas podem ser tratadas individualmente ou como um conjunto denominado bloco.
+		 * Um bloco em Java é um grupo de diretivas delimitadas por chaves ({e}), que define um escopo
+		 * próprio e que recebe um tratamento equivalente ao de uma única diretiva. 
+		 * 
+		 *	// Bloco que equivale a uma única diretiva
+		 *	{ 
+		 *		diretivaA;
+		 *		...
+		 *		diretivaN;
+		 *	} 
+		 *
+		 * As diretivas de controle operam sobre uma diretiva individual ou um bloco de diretivas.
+		 * Outra consequência da criação de blocos é que as estruturas de controle podem ser aninhadas
+		 * de forma recursiva, por exemplo, dentro de um bloco podem existir estruturas de controle que
+		 * contenham seus próprios blocos.  
+		 *
+		 *
+		 */
+			
+
+	}
+
+	
+	
+}
