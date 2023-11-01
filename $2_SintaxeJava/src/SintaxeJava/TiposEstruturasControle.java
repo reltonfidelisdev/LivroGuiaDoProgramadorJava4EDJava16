323a32b9b3d3d4d19804005999f468e8c1a39817
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-11-01 15:39:15 -0300

    Update TiposEstruturasControle.java
    Adicionado exemplo de uso da diretiva switch como expressão.

--- $2_SintaxeJava/src/SintaxeJava/TiposEstruturasControle.java
+++ $2_SintaxeJava/src/SintaxeJava/TiposEstruturasControle.java
@@ -1,6 +1,12 @@
 package SintaxeJava;
 
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class TiposEstruturasControle {
@@ -114,8 +120,8 @@ public class TiposEstruturasControle {
 			}
 			catch(ArrayIndexOutOfBoundsException e) {
 			// trata o erro ArrayIndexOutOfBoundsException
-				System.out.println("Deu erro ao tentar referenciar uma posição inexistente do vetor");
-				System.out.println(e);
+				System.out.println("Erro ao tentar referenciar uma posição inexistente do vetor de notas.");
+				//System.out.println(e);
 			} 
 			finally {
 				System.out.println("Sempre entra aqui no finaly");
@@ -130,22 +136,104 @@ public class TiposEstruturasControle {
 				}
 			} catch (ArrayIndexOutOfBoundsException e1) {
 				// Trata erro ArrayIndexOutOfBoundsException
-				System.err.println("Não passou nenhum argumento");
+				System.err.println("Nao passou nenhum argumento!");
+				System.out.println("Informe uma vogal ou um numero inteiro. \n");
 			} catch (NumberFormatException e2) {
 				// trata erro NumberFormatException 
 				System.out.println("Argumento deve ser um inteiro");
 			}
+
+			/**
+			 * Uso da diretiva switch
+			 * A diretiva switch é um desvio múltiplo de floxo, que avalia uma expressão ordinal
+			 * para escolher um caminho de execução entre vários possíveis. 
+			 * Uma expressão é ordinal quando seu resultado pertence a um conjunto em que são
+			 * conhecidos os elementos anterior e posterior de qualquer um de seus valores.
+			 * Ex:
+			 * (expressao ?
+			 * 	case : 1
+			 * 	printf("continue..");
+			 * 	case : 2
+			 * 	break;
+			 * ) 
+			 *
+			 */
+				// Exemplo diretiva switch
+				if(args.length > 0){ // testa presença de argumentos
+					switch (args[0].charAt(0)) { // Avalia inicial do 1º argumento 
+					case 'a': // Argumento com a inicial minúscula 
+					case 'A': System.out.println("Vogal A");//
+						break;
+					case 'e' :
+					case 'E' : System.out.println("Vogal E");
+						break;
+					case 'i' :
+					case 'I' : System.out.println("Vogal I");
+						break;
+					case 'o' :
+					case 'O' : System.out.println("Vogal O");
+						break;
+					case 'u' :
+					case 'U' : System.out.println("Vogal U");
+						break;
+					default:
+						System.out.println("Nao e uma vogal");
+					}
+				}else {
+					System.out.println("Nao foi fornecido argumento.");
+				}
+				
+				// Diretiva switch com string
+				switch(args[0].toLowerCase()) { // Avalia o texto do 1º argumento em minúsculas
+				case "data" : System.out.println(DateFormat.getDateInstance().
+						format(new Date()));
+				break;
+				case "hora" :
+				System.out.println(DateFormat.getTimeInstance().
+						format(new Date()));
+					break;
+				case "datanova" :
+					System.out.println(LocalDate.now().
+							format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+					break;
+				case "horanova" :
+					System.out.println(LocalTime.now().
+							format(DateTimeFormatter.ofPattern("hh:mm:ss")));
+					
+				default :
+					System.out.println("Comando nao conhecido.");
+				}
+
+				// Diretiva switch como expressão
+				int mes = 1; 
+				int nDias;
+				switch(mes){
+				case 1 :
+				case 3 :
+				case 5 :
+				case 7 :
+				case 8 :
+				case 10:
+				case 12:
+					nDias = 31;
+					break;
+				case 4 : case 6: case 9: case 11:
+					nDias = 30;
+					break;
+				default : 
+					nDias = 28;
+				}
 			
-		/**
-		 * Uso da diretiva break
-		 *
-		 */
+			/**
+			 * Uso da diretiva break
+			 *
+			 */
 			
 		/**
 		 * Uso da diretiva continue
 		 */
 		
-			System.out.println("O programa foi executado até o final");
+			System.out.println("O programa foi executado ate o final");
 
 	}
 
